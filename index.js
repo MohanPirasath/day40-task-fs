de4e79d0e7c1f73b64f9eea198bd43d081ba7d52
Mohanpirasath <mahendran.mohanmass2000@gmail.com>
2022-06-11 23:05:59 +0530

    completed

--- index.js
+++ index.js
@@ -25,6 +25,11 @@ const Time= "The Time is  " + (hours + ":" + min +":" + sec)
 
 
 app.get("/",function(req,res){
+    res.send("give the end point of '/time' to see the time and use '/date' to see the current date and time")
+})
+
+
+app.get("/time",function(req,res){
     fs.writeFile(`${full_date}.txt`,Time,(error)=>console.log("completed"))
     
 // res.send(`${Time}`)
@@ -36,6 +41,25 @@ app.get("/",function(req,res){
         else( res.send(`${data}`))
        }) 
     
+})
+
+
+
+app.get("/date",function(req,res){
+    // fs.writeFile(`${full_date}.txt`,Time,(error)=>console.log("completed"))
+
+    fs.writeFile(`date_time.txt`,
+    (`Date:${full_date} And  ${Time}`),
+    (error)=>console.log("completed"))
+
+    fs.readFile(`date_time.txt`,"utf-8",(error,data)=>{
+        if(error){
+            console.log("error da dai")
+        }
+        else{
+            res.send(data)
+        }
+    })
 })
 
 app.listen(PORT,()=>console.log(`App started in ${PORT}`))
